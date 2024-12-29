b893b2b93002c13270a6cd5d8d3a1ca0377a9832
yogesh16singh <ys1997642@gmail.com>
2024-12-29 18:57:11 +0530

    first commit

---
+++ client/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "randomuser.me",'example.com','i.ytimg.com'],
+  },
+};
+
+module.exports = nextConfig;
